c0906f4448a92bf13434efe69f839206b903e666
VictorrMendes <victo.mendes.souza@gmail.com>
2024-03-13 21:14:29 -0300

    atualizações

--- script.js
+++ script.js
@@ -2,40 +2,37 @@ let inputNovaTarefa = document.querySelector('#inputNovaTarefa');
 let btnAddTarefas = document.querySelector('#btnAddTarefa');
 let listaTarefas = document.querySelector('#listaTarefas');
 
-
-inputNovaTarefa.addEventListener('keypress', (e) =>{
-    
-    if(e.keyCode == 13){
+inputNovaTarefa.addEventListener('keypress', (e) => {
+    if (e.keyCode == 13) {
         let tarefa = {
             nome: inputNovaTarefa.value,
             id: gerarId(),
-        }
-        //TODO:: Adicionar a tarefa ao HTML
+        };
+        adicionarTarefa(tarefa);
     }
 });
 
 btnAddTarefas.addEventListener('click', (e) => {
-
     let tarefa = {
         nome: inputNovaTarefa.value,
         id: gerarId(),
-    }
-    //TODO:: Adicionar a tarefa ao HTML
+    };
+    adicionarTarefa(tarefa);
 });
-
-
 
 function gerarId() {
     return Math.floor(Math.random() * 30000);
 }
 
 function adicionarTarefa(tarefa) {
-    let li = criarTagLi();
+    let li = criarTagLi(tarefa); 
     listaTarefas.appendChild(li);
+    inputNovaTarefa.value = "";
 }
 
-function criarTagLi() {
+function criarTagLi(tarefa) { 
     let li = document.createElement('li');
+    li.id =tarefa.id
 
     let span = document.createElement('span');
     span.classList.add('textoTarefa');
@@ -46,15 +43,37 @@ function criarTagLi() {
     let btnEditar = document.createElement('button');
     btnEditar.classList.add('btnAcao');
     btnEditar.innerHTML = '<i class="fa fa-pencil"  ></i>';
+    btnEditar.setAttribute('onclick' , 'editar('+tarefa.id+')');
 
     let btnExcluir = document.createElement('button');
     btnExcluir.classList.add('btnAcao');
     btnExcluir.innerHTML = ' <i class="fa fa-trash"></i>';
+    btnExcluir.setAttribute('onclick' , 'excluir('+tarefa.id+')');
 
     div.appendChild(btnEditar);
     div.appendChild(btnExcluir);
 
     li.appendChild(span);
-    div.appendChild(div);
-    return li
+    li.appendChild(div); 
+    return li;
+}
+
+ function editar(idTarefa){
+    alert(idTarefa)
+
+    let li = document.getElementById('' + idTarefa + "");
+        if(li){
+            
+        }
+    
+}
+
+function excluir(idTarefa){
+    let confirmacao = window.confirm('Tem certeza que deseja excluir?');
+    if(confirmacao){
+        let li = document.getElementById('' + idTarefa + "");
+        if(li){
+            listaTarefas.removeChild(li);
+        }
+    }
 }
